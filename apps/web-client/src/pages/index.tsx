cefbc9076420d63bd26a9204230f86065216c16f
Abinash Panda <abinash@prodios.com>
2021-08-22 09:25:51 +0530

    feat(login): add form validation in login page

--- apps/web-client/src/pages/index.tsx
+++ apps/web-client/src/pages/index.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 import { AiFillLinkedin } from 'react-icons/ai'
 import Link from 'next/link'
 import Image from 'next/image'
+import { Formik } from 'formik'
+import * as yup from 'yup'
 import { Button, Input } from '@taggedweb/ui'
+
+const validationSchema = yup.object().shape({
+  email: yup.string().email().required('Please enter a valid email'),
+  password: yup
+    .string()
+    .min(8, 'Password is too short - should be 8 chars minimum.')
+    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+    .required('Please enter a password'),
+})
 
 export function Home() {
   return (
@@ -24,25 +35,52 @@ export function Home() {
         <div className="text-xs text-gray-500 relative w-full flex items-center justify-center before:w-full before:absolute before:border-b before:border-gray-200 mb-8">
           <div className="bg-white px-4 inline-block mx-auto relative z-1">OR</div>
         </div>
-        <form>
-          <label className="text-sm text-gray-700 mb-2 block" htmlFor="email">
-            Email
-          </label>
-          <Input placeholder="Enter email" id="email" className="mb-8" />
-          <label className="text-sm text-gray-700 mb-2 block" htmlFor="email">
-            Password
-          </label>
-          <Input placeholder="Enter password" id="password" className="mb-8" type="password" />
-          <div className="flex items-center space-x-4">
-            <Button buttonType="primary">Sign Up with Email</Button>
-            <div className="text-sm text-gray-600">
-              Already a member{' '}
-              <Link href="#">
-                <a href="">Sign in</a>
-              </Link>
-            </div>
-          </div>
-        </form>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={(data) => {
+            console.log(data)
+          }}
+        >
+          {({ handleSubmit, values, handleChange, handleBlur, touched, errors }) => (
+            <form onSubmit={handleSubmit}>
+              <label className="text-sm text-gray-700 mb-2 block" htmlFor="email">
+                Email
+              </label>
+              <Input
+                placeholder="Enter email"
+                id="email"
+                className="mb-8"
+                onChange={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                errorMessage={touched.email ? errors.email : undefined}
+              />
+              <label className="text-sm text-gray-700 mb-2 block" htmlFor="email">
+                Password
+              </label>
+              <Input
+                placeholder="Enter password"
+                id="password"
+                className="mb-8"
+                type="password"
+                onChange={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                errorMessage={touched.password ? errors.password : undefined}
+              />
+              <div className="flex items-center space-x-4">
+                <Button buttonType="primary">Sign Up with Email</Button>
+                <div className="text-sm text-gray-600">
+                  Already a member{' '}
+                  <Link href="#">
+                    <a href="">Sign in</a>
+                  </Link>
+                </div>
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   )
--- libs/ui/src/components/input/input.tsx
+++ libs/ui/src/components/input/input.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import React, { forwardRef } from 'react'
+import { HiX, HiCheck } from 'react-icons/hi'
 
 type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
   errorMessage?: string
@@ -11,12 +12,35 @@ function InputComponent(
   ref: React.Ref<HTMLInputElement>,
 ) {
   return (
-    <input
-      className={clsx('border w-full text-sm rounded-md px-4 py-2', className)}
-      style={style}
-      ref={ref}
-      {...restProps}
-    />
+    <div className={clsx('relative', className)} style={style}>
+      <input
+        className={clsx(
+          'border w-full text-sm rounded-md px-4 py-2 text-gray-700',
+          (() => {
+            if (success) {
+              return 'border-success'
+            }
+
+            if (errorMessage) {
+              return 'border-error text-error'
+            }
+
+            return undefined
+          })(),
+        )}
+        ref={ref}
+        {...restProps}
+      />
+      {errorMessage && (
+        <>
+          <div className="text-xs text-error mt-1">{errorMessage}</div>
+          <HiX size={16} className="text-error absolute top-3 right-3 pointer-events-none" />
+        </>
+      )}
+      {success && !errorMessage ? (
+        <HiCheck size={16} className="text-success absolute top-3 right-3 pointer-events-none" />
+      ) : null}
+    </div>
   )
 }
 
