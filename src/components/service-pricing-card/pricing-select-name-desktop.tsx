a1571a7e0fde86fef321309dcb7951858d6510dd
zogmwa <zogmwatech@gmail.com>
2021-10-03 10:34:20 -0700

    Fixed pricing content in desktop screen

--- src/components/service-pricing-card/pricing-select-name-desktop.tsx
+++ src/components/service-pricing-card/pricing-select-name-desktop.tsx
@@ -9,7 +9,7 @@ type ServicePricingProps = {
 
 function PricingSelectNameDesktopComponent({ pricePlans, selected, onSelected }: ServicePricingProps) {
   return (
-    <div className="hidden md:px-2 md:flex md:flex-col md:w-44">
+    <div className="hidden md:px-4 md:flex md:flex-col md:w-48 md:justify-center">
       {pricePlans.map((item, index) => (
         <div
           className={
