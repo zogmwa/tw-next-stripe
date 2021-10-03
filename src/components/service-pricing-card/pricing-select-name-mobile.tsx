5ee2c4c65c2b3cfb878616ea6bbb4f5a5db38dd7
zogmwa <zogmwatech@gmail.com>
2021-10-03 10:54:56 -0700

    Removed useless div

--- src/components/service-pricing-card/pricing-select-name-mobile.tsx
+++ src/components/service-pricing-card/pricing-select-name-mobile.tsx
@@ -9,22 +9,20 @@ type ServicePricingProps = {
 
 function PricingSelectNameMobileComponent({ pricePlans, selected, onSelected }: ServicePricingProps) {
   return (
-    <div className="block md:hidden">
-      <div className="flex justify-around h-8 py-1 my-1 mb-2 bg-gray-100 rounded-xl md:hidden">
-        {pricePlans.map((item, index) => (
-          <div
-            className={
-              selected === index
-                ? 'flex items-center h-full px-4 bg-white rounded-lg'
-                : 'flex items-center h-full px-4 cursor-pointer'
-            }
-            key={item.name}
-            onClick={() => onSelected(index)}
-          >
-            {item.name}
-          </div>
-        ))}
-      </div>
+    <div className="flex justify-around h-8 py-1 my-1 mb-2 bg-gray-100 rounded-xl md:hidden">
+      {pricePlans.map((item, index) => (
+        <div
+          className={
+            selected === index
+              ? 'flex items-center h-full px-4 bg-white rounded-lg'
+              : 'flex items-center h-full px-4 cursor-pointer'
+          }
+          key={item.name}
+          onClick={() => onSelected(index)}
+        >
+          {item.name}
+        </div>
+      ))}
     </div>
   )
 }
