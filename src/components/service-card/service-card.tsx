c810ed5298409a3bb4fa401444782bfa5db57584
Sumit <sumitkumarsingh.met18@itbhu.ac.in>
2021-10-03 18:38:11 +0530

    refactor(ui): add mobile-view for service-card

--- src/components/service-card/service-card.tsx
+++ src/components/service-card/service-card.tsx
@@ -1,7 +1,9 @@
 import React, { useMemo } from 'react'
 import { BsChevronUp } from 'react-icons/bs'
-import { AiFillStar, AiOutlineCheckCircle } from 'react-icons/ai'
+import { AiOutlineCheckCircle, AiFillStar } from 'react-icons/ai'
 import { FiUsers } from 'react-icons/fi'
+import { GrShare } from 'react-icons/gr'
+import numeral from 'numeral'
 import { TruncatedDescription } from '../truncated-description'
 import { Button } from '../button'
 import { Asset } from '../../types/asset'
@@ -28,22 +30,38 @@ function ServiceCardComponent({ service, onToggleCompare }: ServiceCardProps) {
   }, [service.avg_rating])
 
   return (
-    <div className="flex flex-col px-4 py-4 space-y-3 md:flex-row md:space-x-8 md:space-y-0">
-      <div className="flex items-start justify-start w-full space-x-8">
+    <div className="flex flex-col pt-4 space-y-3 md:flex-row md:space-x-8 md:space-y-0 service-detail-card">
+      <div className="flex items-start justify-start w-full space-x-4 md:space-x-8">
         <div className="flex flex-col items-center justify-start space-y-3">
           <img src={service.logo_url} alt="Web Service" className="object-contain h-[72px] w-[72px] rounded-md" />
           <div className="flex items-center space-x-2">
             <Checkbox onChange={onCompare} />
-            <div className="text-xs text-text-tertiary uppercase">Compare</div>
+            <div className="text-xs uppercase text-text-tertiary">Compare</div>
           </div>
         </div>
         <div className="flex-1">
-          <h1 className="text-base font-medium text-text-primary">{service.name}</h1>
-          <TruncatedDescription description={service.description} className="mb-2" />
-          <div className="flex flex-row flex-wrap mb-5 space-x-2">
+          <div className="flex justify-start space-x-2">
+            <h1 className="text-base font-medium text-text-primary">{service.name}</h1>
+            <a
+              href={service.website ?? '#'}
+              target={service.website ? '_blank' : ''}
+              className="self-center"
+              rel="noreferrer"
+            >
+              <Button
+                className="hidden md:inline-flex"
+                size="small"
+                icon={<GrShare className="gr-primary gr-icon-share" />}
+              >
+                Visit Website
+              </Button>
+            </a>
+          </div>
+          {service.short_description ? <TruncatedDescription description={service.short_description} /> : null}
+          <div className="flex flex-row flex-wrap mb-5">
             {service.tags.map((tag) => {
               return (
-                <Button key={tag.slug} buttonType="tag" size="small">
+                <Button key={tag.slug} buttonType="tag" size="small" className="mt-2 mr-2">
                   {tag.name}
                 </Button>
               )
@@ -52,11 +70,11 @@ function ServiceCardComponent({ service, onToggleCompare }: ServiceCardProps) {
           <div className="flex items-center space-x-6 text-sm">
             <div className="flex items-center space-x-2">
               <BsChevronUp className="text-primary" />
-              <p className="text-text-secondary">{service.upvotes_count}</p>
+              <p className="text-text-secondary">{numeral(service.upvotes_count).format('0.[0]a')}</p>
             </div>
             <div className="flex items-center space-x-2">
               <FiUsers className="text-primary" />
-              <p className="text-text-secondary">{service.users_count} Users</p>
+              <p className="text-text-secondary">{numeral(service.users_count).format('0.[0]a')} Users</p>
             </div>
             {service.has_free_trial === true && (
               <div className="flex items-center space-x-2">
@@ -66,14 +84,18 @@ function ServiceCardComponent({ service, onToggleCompare }: ServiceCardProps) {
             )}
           </div>
         </div>
-        <div className="flex items-start justify-between md:flex-col md:space-y-2 md:items-center md:justify-center">
-          <div className="flex items-center space-x-2">
-            <AiFillStar className="text-primary" />
-            <span className="mr-1 text-2xl font-bold">{rating}</span>
-            <span>/ 10</span>
-          </div>
-          <p className="text-sm text-text-secondary">{service.reviews_count} Reviews</p>
+      </div>
+      <div className="flex flex-row justify-center space-x-4 md:flex-col md:justify-start md:items-center md:space-x-0 md:space-y-4">
+        <div className="flex inline-flex items-center w-40 space-x-2 md:hidden">
+          <AiFillStar className="text-primary" />
+          <span className="mr-1 text-2xl font-bold">{rating}</span>
+          <span>/ 10</span>
         </div>
+        <div className="items-center hidden space-x-1 md:inline-flex">
+          <AiFillStar className="text-primary" />
+          <div className="flex mr-1 text-2xl font-bold">{rating}</div>
+        </div>
+        <p className="inline-flex text-sm text-text-secondary">{service.reviews_count} Reviews</p>
       </div>
     </div>
   )
