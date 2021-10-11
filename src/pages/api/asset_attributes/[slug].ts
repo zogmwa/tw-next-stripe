40792818f82e701a8d5bc6a5349910c5706d5483
zogmwa <zogmwatech@gmail.com>
2021-10-11 10:25:52 -0700

    Fixed fetch voted attributes URL

--- src/pages/api/asset_attributes/[slug].ts
+++ src/pages/api/asset_attributes/[slug].ts
@@ -9,7 +9,7 @@ export default withSessionApi(async (req, res) => {
   if (req.method === 'GET') {
     const { slug } = req.query
     const access = await getAccessToken(req.session)
-    const { data } = await client.get(`/asset_attributes/?assets__slug=${slug}`, {
+    const { data } = await client.get(`/asset_attributes/?assets__slug=${slug}&asset=${slug}`, {
       headers: {
         Authorization: `Bearer ${access}`,
       },
