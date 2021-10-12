ce57f4b3a6f8a9640b3da1e3a933dab32bfbb80c
zogmwa <zogmwatech@gmail.com>
2021-10-12 09:17:33 -0700

    Fixed add a question component in storybook

--- src/components/add-a-question/add-a-question.tsx
+++ src/components/add-a-question/add-a-question.tsx
@@ -7,17 +7,48 @@ import { Textarea } from '../textarea'
  */
 type questionProps = {
   setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>
+  addQuestionName?: string
+  setAddQuestionName?: React.Dispatch<React.SetStateAction<string>>
+  addQuestionNameErrorMessage?: string
+  addQuestionAction?: Function
 }
 
-function AddAQuestionComponent({ setIsOpen }: questionProps) {
+function AddAQuestionComponent({
+  setIsOpen,
+  addQuestionName,
+  setAddQuestionName,
+  addQuestionNameErrorMessage,
+  addQuestionAction,
+}: questionProps) {
   return (
     <div>
       <div className="flex">
         <div className="flex-grow font-bold">Post your question</div>
       </div>
-      <Textarea id="title" name="title" className="w-full mt-4 mb-4" placeholder="Please add your Question?" />
+      <Textarea
+        id="title"
+        name="title"
+        className="w-full mt-4 mb-4"
+        placeholder="Please add your Question?"
+        value={addQuestionName}
+        onChange={(evnet) => {
+          const target = evnet.target as HTMLTextAreaElement
+          setAddQuestionName(target.value)
+        }}
+        errorMessage={addQuestionNameErrorMessage}
+      />
       <div className="flex flex-row-reverse">
-        <Button className="ml-4" buttonType="primary" type="submit">
+        <Button
+          className="ml-4"
+          buttonType="primary"
+          type="submit"
+          onClick={async () => {
+            await addQuestionAction()
+            if (addQuestionName !== '') {
+              setIsOpen(false)
+            }
+          }}
+        >
           Add
         </Button>
         <Button buttonType="default" type="submit" onClick={() => setIsOpen(false)}>
--- src/components/add-a-question/add-ad-question.stories.tsx
+++ src/components/add-a-question/add-ad-question.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Meta } from '@storybook/react/types-6-0'
 import { AddAQuestion } from './add-a-question'
 
@@ -8,5 +8,24 @@ export default {
 } as Meta
 
 export function DefaultAddAHighlight() {
-  return <AddAQuestion />
+  const [addQuestionName, setAddQuestionName] = useState('')
+  const [addQuestionNameErrorMessage, setAddQuestionNameErrorMessage] = useState('')
+
+  const addQuestionAction = async () => {
+    if (addQuestionName === '') {
+      setAddQuestionNameErrorMessage('This field is valid')
+    } else {
+      setAddQuestionNameErrorMessage('')
+      setAddQuestionName('')
+    }
+  }
+
+  return (
+    <AddAQuestion
+      addQuestionName={addQuestionName}
+      setAddQuestionName={setAddQuestionName}
+      addQuestionNameErrorMessage={addQuestionNameErrorMessage}
+      addQuestionAction={addQuestionAction}
+    />
+  )
 }
