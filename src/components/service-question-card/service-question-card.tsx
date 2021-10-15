b3113bfdaf8e0fe30966876491f642f80b5a6cca
zogmwa <zogmwatech@gmail.com>
2021-10-14 18:36:10 -0700

    Updated resuable service question card and question editor

--- src/components/service-question-card/service-question-card.tsx
+++ src/components/service-question-card/service-question-card.tsx
@@ -8,17 +8,64 @@ import { Button } from '../button'
 
 type ServiceQuestionCardProps = {
   question: ServiceQuestion
+  votedQuestions: { id?: number; question?: number }[]
+  upvoteQuestion: Function
+  isLoading: boolean
+  clickedQuestionId: number
+  setClickedQuestionId: Function
 }
 
-function ServiceQuestionCardComponent({ question }: ServiceQuestionCardProps) {
+function ServiceQuestionCardComponent({
+  question,
+  votedQuestions,
+  upvoteQuestion,
+  isLoading,
+  clickedQuestionId,
+  setClickedQuestionId,
+}: ServiceQuestionCardProps) {
+  let renderButton = (
+    <Button
+      icon={<FaThumbsUp />}
+      size="small"
+      onClick={() => {
+        upvoteQuestion(question.id, true)
+        setClickedQuestionId(question.id)
+      }}
+      disabled={isLoading}
+      loading={clickedQuestionId === question.id && isLoading}
+      loadingClassName="w-3 h-3"
+    >
+      Helpful
+    </Button>
+  )
+  votedQuestions
+    .filter((votedQuestion) => votedQuestion?.question === question.id)
+    .map((showQuestion) => {
+      renderButton = (
+        <Button
+          icon={<FaThumbsUp className="text-white" />}
+          size="small"
+          className="bg-primary"
+          textClassName="text-white"
+          onClick={() => {
+            upvoteQuestion(showQuestion?.id, false)
+            setClickedQuestionId(question.id)
+          }}
+          disabled={isLoading}
+          loading={clickedQuestionId === question.id && isLoading}
+          loadingClassName="text-background-light w-3 h-3"
+        >
+          Helpful
+        </Button>
+      )
+    })
+
   return (
     <div className="flex flex-col w-full space-y-3">
       <div className="text-sm font-medium text-text-primary">{question.title}</div>
       <QuestionTruncated description={question.primary_answer} />
       <div className="flex items-center space-x-4 text-xs font-medium text-text-tertiary">
-        <Button icon={<FaThumbsUp />} size="small">
-          Helpful
-        </Button>
+        {renderButton}
         <div>{question.upvotes_count} people found this question helpful</div>
         <div className="hidden md:flex md:space-x-3">
           <BsDot className="self-center" />
