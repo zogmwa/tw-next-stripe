7e3d3fdf68dbb1f525b42342411eeb6bd3d3a165
zogmwa <zogmwatech@gmail.com>
2021-10-15 12:38:02 -0700

    Updated related card for API integration

--- src/components/related-product-card/related-product-card.tsx
+++ src/components/related-product-card/related-product-card.tsx
@@ -3,16 +3,24 @@ import { Checkbox } from '../checkbox'
 import { MAX_RELATEDCARDCONTENT_LENGTH } from '../../utils/constants'
 
 function RelatedProductCardComponent({ relatedProduct }) {
-  const [checked, setChecked] = useState(relatedProduct.isCompare)
+  const [checked, setChecked] = useState(false)
 
   return (
     <div className="flex flex-col items-center p-4 mt-2 mr-2 border border-solid border-border-default">
-      <img src={relatedProduct.imageUrl} alt="Related" className="object-cover rounded-md max-w-16 max-h-16" />
-      <span className="mt-4 text-sm text-center text-bold">{relatedProduct.title}</span>
+      {relatedProduct.logo_url ? (
+        <img
+          src={relatedProduct.logo_url}
+          alt={`${relatedProduct.name}`}
+          className="object-cover w-16 h-16 rounded-md"
+        />
+      ) : (
+        <div className="object-cover w-16 h-16 rounded-md" />
+      )}
+      <span className="mt-4 text-sm text-center text-bold">{relatedProduct.name}</span>
       <span className="mt-4 text-sm text-center text-text-tertiary">
-        {relatedProduct?.content.length < MAX_RELATEDCARDCONTENT_LENGTH
-          ? relatedProduct?.content
-          : `${relatedProduct?.content.substring(0, MAX_RELATEDCARDCONTENT_LENGTH)}...`}
+        {relatedProduct?.description.length < MAX_RELATEDCARDCONTENT_LENGTH
+          ? relatedProduct?.description
+          : `${relatedProduct?.description.substring(0, MAX_RELATEDCARDCONTENT_LENGTH)}...`}
       </span>
       <div className="flex items-center space-x-1.5 mt-4">
         <Checkbox size="md" id={relatedProduct.id} checked={checked} onChange={(e) => setChecked(e.target.checked)} />
