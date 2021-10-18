9f3a4d4ce678e26c3c817153b0c9468338d46d70
zogmwa <zogmwatech@gmail.com>
2021-10-18 07:04:42 -0700

    Fixed image alt text

--- src/components/user-avatar/user-avatar.tsx
+++ src/components/user-avatar/user-avatar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 type UserAvatarType = {
   user: {
     avatar: string
+    username: string
     first_name: string
     last_name: string
   }
@@ -12,7 +13,7 @@ function UserAvatarComponent({ user, size }: UserAvatarType) {
   return (
     <>
       {user.avatar ? (
-        <img src={user.avatar} alt="user" className={`object-cover rounded-md w-${size} h-${size}`} />
+        <img src={user.avatar} alt={user.username} className={`object-cover rounded-md w-${size} h-${size}`} />
       ) : (
         <div
           className={`flex items-center justify-center w-${size} h-${size} bg-gray-200 rounded-full focus-visible:ring-2 !focus:outline-none !shadow-none focus-visible:ring-white focus-visible:ring-opacity-75`}
