e0b09110d4ce0199e86af1104b885392cdef890b
zogmwa <zogmwatech@gmail.com>
2021-10-18 14:28:58 -0700

    Added compare services detail card to storybook

---
+++ src/components/compare/services-detail/services-compare-card.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Asset } from '../../../types/asset'
+import { Button } from '../../button'
+
+type ServicesCompareCard = {
+  services: Asset[]
+}
+
+function ServicesCompareCardComponent({ services }: ServicesCompareCard) {
+  const serivceCount = services.length
+
+  return (
+    <>
+      <div className="flex flex-col items-start justify-between md:flex-row md:items-center">
+        <div className="flex items-end">
+          {services.map((service, index) => (
+            <>
+              <h2>{service.name}</h2>
+              {serivceCount - 1 !== index && <span className="px-4 text-sm text-text-secondary">Vs</span>}
+            </>
+          ))}
+        </div>
+        <Button buttonType="primary" textClassName="text-background-light" className="mt-2 md:mt-0">
+          Compare another Product
+        </Button>
+      </div>
+      <div className="flex items-end mt-4">
+        {services.map((service, index) => (
+          <>
+            <span className="text-sm text-text-secondary">{service.name}</span>
+            {serivceCount - 1 !== index && <span className="px-1 text-sm text-text-secondary">vs</span>}
+            {serivceCount - 1 === index && <span className="pr-1 text-sm text-text-secondary">:</span>}
+          </>
+        ))}
+        <span className="text-sm text-text-secondary">What are the differences?</span>
+      </div>
+      {services.map((service) => (
+        <div className="mt-2">
+          <span className="pr-1 text-sm font-semibold">What is {service.name}?</span>
+          <span className="text-sm text-text-secondary">{service.description}</span>
+        </div>
+      ))}
+      <div className="mt-4 text-sm text-text-secondary">
+        Both are tools in the
+        <span className="font-semibold text-black"> Web </span>
+        and
+        <span className="font-semibold text-black"> Video Conferencing </span>
+        category of a tech stack.
+      </div>
+    </>
+  )
+}
+
+export const ServicesCompareCard = ServicesCompareCardComponent
