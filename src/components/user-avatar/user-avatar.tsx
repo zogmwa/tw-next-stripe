7192036e2e612e4d1baff4ffbb40c63e1aa05131
zogmwa <zogmwatech@gmail.com>
2021-10-18 07:00:07 -0700

    Updated user avatar with user object

--- src/components/user-avatar/user-avatar.tsx
+++ src/components/user-avatar/user-avatar.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { BsPersonFill } from 'react-icons/bs'
 
-type AvatarUrl = {
-  avatarUrl: string
+type UserAvatarType = {
+  user: {
+    avatar: string
+    first_name: string
+    last_name: string
+  }
   size: number
 }
-function UserAvatarComponent({ avatarUrl, size }: AvatarUrl) {
+function UserAvatarComponent({ user, size }: UserAvatarType) {
   return (
     <>
-      {avatarUrl ? (
-        <img src={avatarUrl} alt="user" className={`object-cover rounded-md w-${size} h-${size}`} />
+      {user.avatar ? (
+        <img src={user.avatar} alt="user" className={`object-cover rounded-md w-${size} h-${size}`} />
       ) : (
         <div
           className={`flex items-center justify-center w-${size} h-${size} bg-gray-200 rounded-full focus-visible:ring-2 !focus:outline-none !shadow-none focus-visible:ring-white focus-visible:ring-opacity-75`}
         >
-          <BsPersonFill size={22} color="gray" />
+          <p>{user.first_name[0] + user.last_name[0]}</p>
         </div>
       )}
     </>
