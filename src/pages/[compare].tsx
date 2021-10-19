cab5b6d0895168443a5c19df12cc538aad48b20d
zogmwa <zogmwatech@gmail.com>
2021-10-19 14:50:21 -0700

    Added favicon for fix issue

--- src/pages/[compare].tsx
+++ src/pages/[compare].tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { fetchServicesDetailCompareServer } from '../server-queries/fetch-services-compare-detail'
 
 export const getServerSideProps = async ({ query }) => {
-  const services = query.services
+  /*
+    ** https://github.com/vercel/next.js/discussions/13301
+    ** Because of this I added favicon to /public
+  */
+  const { services: serviceSlugs } = query
+  const servicesUrl = '?asset__slugs=' + serviceSlugs.join('&asset__slugs=')
+  const services = await fetchServicesDetailCompareServer(servicesUrl)
   return {
-    props: {
-      services,
-    },
+    props: { services },
   }
 }
 
-export default function CompareList(services) {
-  console.log(services)
+export default function CompareList({ services }) {
+
   return <></>
 }
