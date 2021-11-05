3c6594de88e993b86d50027acc1234d8955710c2
zogmwa <zogmwatech@gmail.com>
2021-11-05 11:35:02 -0700

    Updated editable carousel component for edit carousel

--- src/components/editable-components/editable-carosel.tsx
+++ src/components/editable-components/editable-carosel.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react'
 import dynamic from 'next/dynamic'
 import { ProductContentCarouselPropsType } from '../service-detail/product-content-carousel'
 import { ProductContentCarouselSkeleton } from '../service-detail/product-content-skeleton'
-import { ShowEditable } from './index'
 import { Modal } from '../Modal'
 import { ImageUploader } from '../image-uploader'
 import { fetchFileFromCloudinary } from '../../utils/file'
+import { Button } from '../button'
 
 export type EditableCarouselComponentType = {
   promo_video: string
@@ -13,6 +13,9 @@ export type EditableCarouselComponentType = {
     asset: number
     url: string
   }[]
+  onChange: Function
+  isEdit: boolean
+  onEdit: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const ProductContentCarousel = dynamic<ProductContentCarouselPropsType>(
@@ -23,22 +26,31 @@ const ProductContentCarousel = dynamic<ProductContentCarouselPropsType>(
   },
 )
 
-function EditableCarouselComponent({ promo_video, images }: EditableCarouselComponentType) {
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false)
-  const [isUploadinImage, setIsUploadingImage] = useState(false)
+function EditableCarouselComponent({ promo_video, images, onChange, isEdit, onEdit }: EditableCarouselComponentType) {
+  const [isUploadingImage, setIsUploadingImage] = useState(false)
   const [loadedFilesList, setLoadedFilesList] = useState([])
   const [loadedFilesUrls, setLoadedFilesUrls] = useState({})
-  const [promoVideoUrl, setPromoVideoUrl] = useState(promo_video)
+  const [updateUrls, setUpdateUrls] = useState([])
+  const [promoVideoUrl, setPromoVideoUrl] = useState('')
+  const [defaultImages, setDefaultImages] = useState([])
+
+  useEffect(() => {
+    let defaultUpdateUrl = []
+    for (let i = 0; i < images.length; i++) defaultUpdateUrl.push({ url: images[i].url })
+    setDefaultImages(images)
+    setUpdateUrls(defaultUpdateUrl)
+    setPromoVideoUrl(promo_video)
+  }, [images, promo_video])
 
   useEffect(() => {
     async function getImageFilesList() {
-      await Promise.all(images.map((image) => fetchFileFromCloudinary(image.url))).then((res) => {
+      await Promise.all(defaultImages.map((image) => fetchFileFromCloudinary(image.url))).then((res) => {
         let loadedFiles = []
         let loadedUrls = {}
         for (let i = 0; i < res.length; i++) {
           let loadedFile: any = res[i] as File
           loadedFile.name = `fake-loaded-file-name-${i}`
-          loadedUrls[`fake-loaded-file-name-${i}`] = images[i].url
+          loadedUrls[`fake-loaded-file-name-${i}`] = defaultImages[i].url
           loadedFiles.push(loadedFile)
         }
 
@@ -48,17 +60,34 @@ function EditableCarouselComponent({ promo_video, images }: EditableCarouselComp
     }
 
     getImageFilesList()
-  }, [isEditModalOpen])
+  }, [isEdit, images])
+
+  const handleSubmit = () => {
+    const updateData = {
+      snapshots: updateUrls,
+      promo_video: promoVideoUrl,
+    }
+
+    onChange(updateData)
+    onEdit(false)
+  }
 
   return (
     <>
-      <div className="flex">
-        <ShowEditable onEdit={() => setIsEditModalOpen(true)}>
-          <ProductContentCarousel promo_video={promo_video} images={images} />
-        </ShowEditable>
-      </div>
-      <Modal isOpen={isEditModalOpen} setIsOpen={setIsEditModalOpen} size="xl">
+      <ProductContentCarousel promo_video={promoVideoUrl} images={defaultImages} />
+      <Modal isOpen={isEdit} setIsOpen={onEdit} size="xl">
         <>
+          <div className="flex items-center pb-2">
+            <label className="pr-2 text-sm font-medium text-text-primary">Video URL</label>
+            <input
+              className="px-2 border border-solid rounded-md border-border-default focus:ring-0"
+              placeholder="https://example.com/video.mp4"
+              name="promo-video"
+              value={promoVideoUrl}
+              onChange={(e) => setPromoVideoUrl(e.target.value)}
+            />
+          </div>
+          <label className="text-sm font-medium text-text-primary">Snapshots</label>
           <ImageUploader
             limit={5}
             defaultUrls={loadedFilesUrls}
@@ -67,23 +96,25 @@ function EditableCarouselComponent({ promo_video, images }: EditableCarouselComp
               setLoadedFilesList(files)
             }}
             onChange={(urls) => {
-              // setFieldValue(
-              //   'snapshots',
-              //   urls.map((url) => ({ url })),
-              // )
-              console.log('urls', urls)
+              setUpdateUrls(urls.map((url) => ({ url })))
             }}
             files={loadedFilesList}
           />
-          <label className="block mb-2 text-sm font-medium lg:text-base text-text-primary" htmlFor="description">
-            Video URL
-          </label>
-          <input
-            placeholder="https://example.com/video.mp4"
-            name="promo-video"
-            value={promo_video}
-            onChange={(e) => setPromoVideoUrl(e.target.value)}
-          />
+          <div className="flex flex-row-reverse mt-2">
+            <Button className="mr-2" disabled={isUploadingImage} onClick={() => onEdit(false)}>
+              Cancel
+            </Button>
+            <Button
+              className="mr-2 bg-primary"
+              textClassName="text-white"
+              disabled={isUploadingImage}
+              loading={isUploadingImage}
+              loadingClassName="text-background-light"
+              onClick={() => handleSubmit()}
+            >
+              Save
+            </Button>
+          </div>
         </>
       </Modal>
     </>
