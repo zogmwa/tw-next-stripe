a662b2cc4c99df98b8db454f92965a84db0271fc
zogmwa <zogmwatech@gmail.com>
2021-11-04 23:36:37 -0700

    Updated product information section for edit

--- src/components/service-detail/product-content.tsx
+++ src/components/service-detail/product-content.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 import { Asset } from '../../types/asset'
 import { ProductContentCarouselPropsType } from './product-content-carousel'
 import { ProductContentCarouselSkeleton } from './product-content-skeleton'
+import { EditableCarousel } from '../editable-components/'
 
 const ProductContentCarousel = dynamic<ProductContentCarouselPropsType>(
   () => import('./product-content-carousel').then((mod) => mod.ProductContentCarousel),
@@ -13,9 +14,10 @@ const ProductContentCarousel = dynamic<ProductContentCarouselPropsType>(
 )
 type ServiceDetailPriductProps = {
   service: Asset
+  editAllowed?: boolean
 }
 
-function ProductContentComponent({ service }: ServiceDetailPriductProps) {
+function ProductContentComponent({ service, editAllowed = false }: ServiceDetailPriductProps) {
   if (typeof service === 'undefined') return null
 
   const promo_video = service?.promo_video ?? ''
@@ -25,7 +27,11 @@ function ProductContentComponent({ service }: ServiceDetailPriductProps) {
     <div className="ml-3">
       <h1 className="text-base font-medium text-text-primary">What is {service.name}?</h1>
       <p className="space-y-2 text-sm text-text-secondary">Screenshots</p>
-      <ProductContentCarousel promo_video={promo_video} images={images} />
+      {editAllowed ? (
+        <EditableCarousel promo_video={promo_video} images={images} />
+      ) : (
+        <ProductContentCarousel promo_video={promo_video} images={images} />
+      )}
       <p className="mt-2 space-y-2 text-sm text-text-secondary">{service.description}</p>
     </div>
   )
