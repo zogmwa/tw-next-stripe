8f8584c57d1d7d2e7501342b9e76594b59ae6929
zogmwa <zogmwatech@gmail.com>
2021-11-08 21:14:21 -0800

    Added organization autocomplete to edit page

---
+++ src/components/editable-components/editable-usedbycompanies/link-usedbycompanies-bar.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import AsyncSelect from 'react-select/async'
+import axios from 'axios'
+
+const filterOrganizations = async (inputValue: string) => {
+  const { data } = await axios.get(`/api/autocomplete/organizations/${inputValue}`)
+  const organizations = data.results
+  const tempOrganizations = organizations.filter((organization) =>
+    organization.name.toLowerCase().includes(inputValue.toLowerCase()),
+  )
+  let returnOrganizations = []
+  tempOrganizations.map((organization) =>
+    returnOrganizations.push({
+      label: organization.name,
+      value: organization.name.toLowerCase(),
+      website: organization.website,
+      logo_url: organization.logo_url,
+    }),
+  )
+
+  return returnOrganizations
+}
+
+const promiseOptions = async (inputValue: string) => {
+  if (inputValue.trim().length >= 3) {
+    let showOrganizations = await filterOrganizations(inputValue)
+
+    return showOrganizations
+  } else {
+    console.log('Input less than 3 letters')
+  }
+}
+
+export default function LinkUsedByCompaniesBar({ onChange }) {
+  return (
+    <AsyncSelect
+      cacheOptions
+      defaultOptions
+      onChange={(value) => {
+        onChange(value)
+      }}
+      loadOptions={promiseOptions}
+      placeholder="Start typing top organizations."
+      components={{ DropdownIndicator: () => null }}
+      className="flex-1 mb-2 md:mb-0"
+      classNamePrefix="select"
+      instanceId
+      name="add-organiztion"
+      menuPortalTarget={document.body}
+      styles={{
+        // Fixes the overlapping problem of the component
+        menuPortal: (base) => ({ ...base, zIndex: 99999999 }),
+      }}
+    />
+  )
+}
---
+++ src/components/editable-components/editable-usedbycompanies/link-usedbycompanies.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from '../../button'
+import { EditableUsedByCompaniesTable } from '../editable-table'
+
+type LinkUsedByCompaniesComponentProps = {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  onSubmit: Function
+  customerOrganizations: { name: string; website: string | null; logo_url: string | null }[]
+}
+
+function LinkUsedByCompaniesComponent({
+  setIsOpen,
+  onSubmit,
+  customerOrganizations,
+}: LinkUsedByCompaniesComponentProps) {
+  const [editCustomerOrganizations, setEditCustomerOrganizations] = useState(customerOrganizations ?? [])
+  const [isSubmit, setIsSubmit] = useState(false)
+  const defaultCustomerOrganizationData = {
+    name: '',
+    logo_url: '',
+    websiet: '',
+  }
+
+  useEffect(() => {
+    if (isSubmit) {
+      console.log('editCustomerOrganizations:', editCustomerOrganizations)
+      let isSend = true
+      for (let i = 0; i < editCustomerOrganizations.length; i++) {
+        if (editCustomerOrganizations[i].name === '') isSend = false
+      }
+      if (isSend) {
+        onSubmit({ customer_organizations: editCustomerOrganizations })
+        setIsOpen(false)
+      }
+    }
+  }, [isSubmit])
+
+  return (
+    <div className="space-y-4">
+      <EditableUsedByCompaniesTable
+        editCustomerOrganizations={editCustomerOrganizations}
+        defaultCustomerOrganizationData={defaultCustomerOrganizationData}
+        setEditCustomerOrganizations={setEditCustomerOrganizations}
+        isSubmit={isSubmit}
+      />
+      <div className="flex flex-row-reverse">
+        <Button className="ml-4" buttonType="primary" type="submit" onClick={() => setIsSubmit(true)}>
+          Save
+        </Button>
+        <Button
+          buttonType="default"
+          type="submit"
+          onClick={() => {
+            setIsOpen(false)
+          }}
+        >
+          Cancel
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export const LinkUsedByCompanies = LinkUsedByCompaniesComponent
