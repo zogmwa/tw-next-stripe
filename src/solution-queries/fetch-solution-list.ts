efdbd6ce837c1734be609b0edb0ff0e4c416cce2
zogmwa <zogmwatech@gmail.com>
2021-11-18 11:23:00 -0800

    Updated fetch solution list URL

--- src/solution-queries/fetch-solution-list.ts
+++ src/solution-queries/fetch-solution-list.ts
@@ -10,14 +10,14 @@ import { getAccessToken } from '../utils/token'
 export async function fetchSolutionList(session, sendUrl) {
   const access = await getAccessToken(session)
   if (access) {
-    const { data } = await client.get(`/autocomplete-solutions/${sendUrl}`, {
+    const { data } = await client.get(`/solutions/search${sendUrl}`, {
       headers: {
         Authorization: `Bearer ${access}`,
       },
     })
     return data
   } else {
-    const { data } = await client.get(`/autocomplete-solutions/${sendUrl}`)
+    const { data } = await client.get(`/solutions/search${sendUrl}`)
     return data
   }
 }
