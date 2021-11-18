f630a14f12c3721163d4adbb92075e5635d22aee
zogmwa <zogmwatech@gmail.com>
2021-11-18 11:23:19 -0800

    Added solution list pagination in solution list page

--- src/pages/solutions/[search_keywords].tsx
+++ src/pages/solutions/[search_keywords].tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Pagination from '@mui/material/Pagination'
+import { client } from '@taggedweb/utils/client'
 import { fetchSolutionList } from '@taggedweb/solution-queries/fetch-solution-list'
 import { withSessionSSR } from '@taggedweb/utils/session'
+import { SolutionListingCard } from '@taggedweb/components/solution-listing-card'
 
 export const getServerSideProps = withSessionSSR(async (context) => {
   const {
-    params: { search_keywords },
+    query: { search_keywords },
   } = context
 
   const SplitTheString = (url) => {
@@ -14,21 +17,49 @@ export const getServerSideProps = withSessionSSR(async (context) => {
     }
   }
 
-  let solutionList
+  let solutionData
+  let defaultUrl
   try {
     const keywords = SplitTheString(search_keywords)
-    const sendUrl = '?q=' + keywords.join('&q=')
-    solutionList = await fetchSolutionList(context.req.session, sendUrl)
+    defaultUrl = '?q=' + keywords.join('&q=')
+    const sendUrl = `${defaultUrl}&page=1&offest=0&limit=20`
+    solutionData = await fetchSolutionList(context.req.session, sendUrl)
   } catch (error) {
     // eslint-disable-next-line
     // TODO: Redirect to solution search page.
   }
   return {
-    props: { solutionList },
+    props: { solutionData, defaultUrl },
   }
 })
 
-export default function SolutionList({ solutionList }) {
-  console.log(solutionList)
-  return <div></div>
+export default function SolutionList({ solutionData, defaultUrl }) {
+  const [page, setPage] = useState(1)
+  const [pageLen, setPageLen] = useState(20)
+  const [pageCount, setPageCount] = useState(Math.ceil(solutionData.count / pageLen))
+  const [solutionList, setSolutionList] = useState(solutionData.results)
+  const handlePagination = async (event, pageValue) => {
+    const offset = (page - 1) * pageLen
+    const sendUrl = `${defaultUrl}&page=${page}&offset=${offset}&limit=${pageLen}`
+    try {
+      const { data } = await client.get(`/solutions/search${sendUrl}`)
+      setPage(pageValue)
+      setSolutionList(data.results)
+    } catch (error) {
+      // eslint-disable-next-line
+      console.log(error)
+    }
+  }
+  return (
+    <div className="flex flex-col justify-between">
+      <div className="flex flex-col p-2">
+        {solutionList.map((solution) => (
+          <SolutionListingCard listingData={solution} key={solution.slug} />
+        ))}
+      </div>
+      <div className="flex justify-end p-2">
+        <Pagination page={page} count={pageCount} onChange={handlePagination} />
+      </div>
+    </div>
+  )
 }
