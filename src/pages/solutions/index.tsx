1a67b1fdeecb94aa7f727bec7786bdb88849e152
zogmwa <zogmwatech@gmail.com>
2021-11-22 14:22:24 -0800

    Added `/solutions` page for redirect

---
+++ src/pages/solutions/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { TopSolutionTags } from '@taggedweb/utils/top-tags'
+
+export async function getServerSideProps(context) {
+  const topSaasTagSlugs = []
+  let url
+  if (TopSolutionTags.length > 0) {
+    for (let i = 0; i < TopSolutionTags.length; i++) topSaasTagSlugs.push(TopSolutionTags[i].slug)
+
+    url = topSaasTagSlugs.join('--')
+  }
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: `solutions/${url}`,
+    },
+  }
+}
+
+export default function Software() {
+  return <div>There is no Top Solution Tags.</div>
+}
