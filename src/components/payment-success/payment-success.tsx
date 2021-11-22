08eb255f787f73067b922699db9442a456f3b8cb
zogmwa <zogmwatech@gmail.com>
2021-11-22 11:12:19 -0800

    Added alt attribute to img tag

--- src/components/payment-success/payment-success.tsx
+++ src/components/payment-success/payment-success.tsx
@@ -10,7 +10,7 @@ type PaymentSuccessComponentProps = {
 function PaymentSuccessComponent({ emailAddress, className = '' }: PaymentSuccessComponentProps) {
   return (
     <div className={clsx('flex flex-col items-center', className)}>
-      <img src="/images/congratulations.png" className="w-32 h-32" />
+      <img src="/images/congratulations.png" alt="congratulation-image" className="w-32 h-32" />
       <h4 className="my-4 text-3xl font-bold text-text-primary">Thank you</h4>
       <div className="text-lg text-text-tertiary">
         Your payment was successful. We are on it now. We will follow up with you via email at
