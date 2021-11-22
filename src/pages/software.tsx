931be57205ccb7b18e7bbbc9ebf86588076f0449
zogmwa <zogmwatech@gmail.com>
2021-11-22 14:22:05 -0800

    Added `/software` page for redirect

---
+++ src/pages/software.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { TopSaasTags } from '@taggedweb/utils/top-tags'
+
+export async function getServerSideProps(context) {
+  const topSaasTagSlugs = []
+  let url
+  if (TopSaasTags.length > 0) {
+    for (let i = 0; i < TopSaasTags.length; i++) topSaasTagSlugs.push(TopSaasTags[i].slug)
+
+    url = topSaasTagSlugs.join(',')
+  }
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: `softwares/${url}`,
+    },
+  }
+}
+
+export default function Software() {
+  return <div>There is no Top Saas Tags.</div>
+}
