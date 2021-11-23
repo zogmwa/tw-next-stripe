1a36735b33ba911d0cb46bb17b041aa255755995
zogmwa <zogmwatech@gmail.com>
2021-11-22 21:55:06 -0800

    Added software top solution tags list page

--- src/pages/solutions/index.tsx
+++ src/pages/solutions/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { TopSolutionTags } from '@taggedweb/utils/top-tags'
 
-export async function getServerSideProps(context) {
-  const topSaasTagSlugs = []
-  let url
-  if (TopSolutionTags.length > 0) {
-    for (let i = 0; i < TopSolutionTags.length; i++) topSaasTagSlugs.push(TopSolutionTags[i].slug)
-
-    url = topSaasTagSlugs.join('--')
-  }
-
-  return {
-    redirect: {
-      permanent: false,
-      destination: `solutions/${url}`,
-    },
-  }
-}
-
 export default function Software() {
-  return <div>There are no Top Solution Tags.</div>
+  const router = useRouter()
+
+  return (
+    <div className="flex flex-col max-w-screen-lg mx-auto">
+      <h4 className="self-start my-4 text-lg font-semibold text-text-primary">TOP SOLUTION TAGS</h4>
+      <div className="flex flex-col space-y-2">
+        {TopSolutionTags.map((tag) => (
+          <div
+            className="flex items-center px-4 py-2 space-x-2 border border-solid rounded-md cursor-pointer border-border-default"
+            key={tag.slug}
+            onClick={() => router.push(`/solutions/${tag.slug}`)}
+          >
+            <span>
+              {tag.name}
+              {tag.description && ':'}
+            </span>
+            <span>{tag.description}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
