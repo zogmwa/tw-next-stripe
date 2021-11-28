911255c6650d7e4b619521471ead7824d9e87e67
zogmwa <zogmwatech@gmail.com>
2021-11-27 16:46:20 -0800

    Integrated fetch bookmarked solution list API

--- src/pages/api/solution_bookmarks/index.tsx
+++ src/pages/api/solution_bookmarks/index.tsx
@@ -6,6 +6,15 @@ import { getAccessToken } from '@taggedweb/utils/token'
  * API Route handler for bookmarking on a solution.
  */
 export default withApiAuthRequired(async (req, res) => {
+  if (req.method === 'GET') {
+    const access = await getAccessToken(req.session)
+    const { data } = await clientWithRetries.get('/solution_bookmarks/', {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    })
+    res.json(data)
+  }
   if (req.method === 'POST') {
     const access = await getAccessToken(req.session)
     const { data } = await clientWithRetries.post('/solution_bookmarks/', req.body, {
