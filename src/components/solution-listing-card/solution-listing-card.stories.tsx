2100ca9b2c93aa57fd9a66a6f18afc809f4c1ea7
zogmwa <zogmwatech@gmail.com>
2021-12-04 13:36:22 -0800

    Fixed type issue

--- src/components/solution-listing-card/solution-listing-card.stories.tsx
+++ src/components/solution-listing-card/solution-listing-card.stories.tsx
@@ -42,6 +42,41 @@ const listingData = {
     name: 'Solution Provider Organization',
     logo_url: null,
   },
+  assets: [
+    {
+      id: 1,
+      slug: 'mailchimp',
+      name: 'Mailchimp',
+      logo_url: 'http://logo.clearbit.com/mailchimp.com',
+      logo: null,
+      website: 'http://mailchimp.com/',
+    },
+    {
+      id: 2,
+      slug: 'test',
+      name: 'Test',
+      logo_url: 'http://logo.clearbit.com/campaignmonitor.com',
+      logo: null,
+      website: null,
+    },
+    {
+      id: 3,
+      slug: 'Test1',
+      name: 'test1',
+      logo_url:
+        'https://uploads-ssl.webflow.com/616ec74e8792443f1fa65777/61770851a4e37b287caf6c31_Intropages_LOGO_final-02-p-500.png',
+      logo: null,
+      website: null,
+    },
+    {
+      id: 4,
+      slug: 'test2',
+      name: 'Test2',
+      logo_url: 'https://logo.clearbit.com/clockify.me',
+      logo: null,
+      website: null,
+    },
+  ],
 }
 
 export function DefaultSolutionListingCard() {
