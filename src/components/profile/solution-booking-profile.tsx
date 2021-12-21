1f8cc78917e0432c576d3a3552627a83fa2ab150
zogmwa <zogmwatech@gmail.com>
2021-12-21 12:34:14 -0800

    Renamed solution booking profile component

--- src/components/profile/solution-booking-profile.tsx
+++ src/components/profile/solution-booking-profile.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SolutionBookingCard } from '../solution-booking-card'
 
-function BookedSolutionsProfileComponent({ data }) {
+function SolutionBookingsProfileComponent({ data }) {
   const bookedSolutionsList = data.booked_solutions
   bookedSolutionsList.sort((questionA, questionB) => {
     const dateA = new Date(questionA.created)
@@ -9,18 +9,17 @@ function BookedSolutionsProfileComponent({ data }) {
     return (dateA.getTime() - dateB.getTime()) * -1
   })
   return (
-    <div id="bookmarked-solutions" className="mb-8">
-      <p className="text-base font-bold">Bookmarked Solutions</p>
+    <div id="contracts" className="mb-8">
+      <p className="text-base font-bold">Contracts</p>
       <div className="border border-gray-200 divide-y divide-gray-200 rounded-md">
         {bookedSolutionsList &&
           bookedSolutionsList.map((bookedSolution, index) => {
             if (typeof bookedSolution === 'undefined') return null
-            else
-              return <SolutionBookingCard key={`bookmarked-solution-${index}`} solutionBookingData={bookedSolution} />
+            else return <SolutionBookingCard key={`contract-${index}`} solutionBookingData={bookedSolution} />
           })}
       </div>
     </div>
   )
 }
 
-export const BookedSolutionsProfile = BookedSolutionsProfileComponent
+export const SolutionBookingsProfileProfile = SolutionBookingsProfileComponent
