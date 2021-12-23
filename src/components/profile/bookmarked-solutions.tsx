9999f724fd083788aaaf987a2886b223e619eb87
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-12-23 22:10:27 +0530

    added a page for bookmarked solutions

--- src/components/profile/bookmarked-solutions.tsx
+++ src/components/profile/bookmarked-solutions.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { SolutionListingCard } from '../solution-listing-card'
+import Link from 'next/link'
 
 export const BookmarkedSolutionsProfile = ({ data }) => {
-  const solutionsList = data.bookmarked_solutions
   return (
-    <div id="bookmarked-solutions" className="mb-8">
-      <p className="text-base font-bold">Bookmarked Solutions</p>
-      <div className="border border-gray-200 divide-y divide-gray-200 rounded-md">
-        {solutionsList &&
-          solutionsList.map((solution, index) => {
-            if (typeof solution === 'undefined') return null
-            else return <SolutionListingCard key={`bookmarked-solution-${index}`} listingData={solution.solution} />
-          })}
+    <Link href="profile/bookmarked-solutions">
+      <div id="bookmarked-solutions" className="mb-8 border-b border-gray-300 cursor-pointer md:flex">
+        <p className="text-base font-bold">Bookmarked Solutions</p>
+        <span className="ml-auto">{`${data?.bookmarked_solutions.length ?? 0} Product(s)`}</span>
+        {/* <span className="ml-auto">{`${pendingAssets?.length ?? 0} Product(s)`}</span> */}
       </div>
-    </div>
+      {/* <div id="bookmarked-solutions" className="mb-8 border border-gray-300 rounded cursor-pointer">
+        <p className="text-base font-bold "></p>
+      </div> */}
+    </Link>
   )
 }
---
+++ src/pages/profile/bookmarked-solutions.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { SolutionListingCard } from '../../components/solution-listing-card'
+import { useProfile } from '@taggedweb/hooks/use-profile'
+
+import { Profile } from '@taggedweb/types/profile'
+
+export default function BookmarkedSolutionsList() {
+  const profile = useProfile()
+  const solutionsList = profile && profile.bookmarked_solutions
+  return (
+    <div className="flex flex-col w-1/2 mx-auto my-8">
+      <p className="mb-2 text-lg font-bold">Bookmarked Solutions</p>
+      <div className="w-full mb-4 divide-y divide-gray-200 rounded-md">
+        {solutionsList &&
+          solutionsList.map((solution, index) => {
+            if (typeof solution === 'undefined') return null
+            else return <SolutionListingCard key={`bookmarked-solution-${index}`} listingData={solution.solution} />
+          })}
+        {!solutionsList && <div>No Bookmarked Solutions yet!!</div>}
+      </div>
+    </div>
+  )
+}
