06243ce0af33d19fdb27f56c06a9d5bc650d730a
Pranjal Mittal <mittal.pranjal@gmail.com>
2021-12-23 00:42:19 -0500

    Updated the text content on the payment cancel page, also fix an eslint precommit hook error while at it

--- src/__test__/mock-next-test-router.tsx
+++ src/__test__/mock-next-test-router.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 
 export function withTestRouter(tree, router) {
-  const { route = '', pathname = '', query = {}, asPath = '' } = router
+  // If we extract these values and don't use them ESLint throws an error, commenting it out in case we want to use them later
+  // const { route, pathname, query, asPath } = router
 
   return <RouterContext.Provider value={router}>{tree}</RouterContext.Provider>
 }
