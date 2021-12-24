81ae7cd1bc177342d858f1e91522c97e6cedbb39
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-12-25 03:36:26 +0530

    added message when there are no bookmarked solutions

--- src/pages/profile/bookmarked-solutions.tsx
+++ src/pages/profile/bookmarked-solutions.tsx
@@ -8,15 +8,22 @@ export default function BookmarkedSolutionsList() {
   const profile = useProfile()
   const solutionsList = profile && profile.bookmarked_solutions
   return (
-    <div className="flex flex-col w-1/2 mx-auto my-8">
+    <div className="flex flex-col w-3/4 mx-auto xl:w-1/2 my-4 lg:my-8 min-h-[50%] ">
       <p className="mb-2 text-lg font-bold">Bookmarked Solutions</p>
       <div className="w-full mb-4 rounded-md">
+        {solutionsList && solutionsList.length == 0 && (
+          <p className="text-center">
+            No Bookmarked Solutions yet... Explore some solutions{' '}
+            <a href="/" className="underline">
+              here
+            </a>
+          </p>
+        )}
         {solutionsList &&
           solutionsList.map((solution, index) => {
             if (typeof solution === 'undefined') return null
             else return <SolutionListingCard key={`bookmarked-solution-${index}`} listingData={solution.solution} />
           })}
-        {!solutionsList && <div>No Bookmarked Solutions yet!!</div>}
       </div>
     </div>
   )
