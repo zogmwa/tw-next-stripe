a4afd2176affe413a39d46083c50fc71a232e5f9
Pranjal Mittal <mittal.pranjal@gmail.com>
2022-01-02 16:22:43 -0500

    Rename Solution Providers to List your Solution (#747)

--- src/components/navbar-menu/navbar-menu.tsx
+++ src/components/navbar-menu/navbar-menu.tsx
@@ -19,7 +19,7 @@ export function NavbarMenu() {
             rel="noopener noreferrer"
             className="flex px-4 py-2 text-sm text-base tracking-wide rounded cursor-pointer text-primary hover:bg-gray-200"
           >
-            Solution Providers
+            List your Solution
           </a>
         </Link>
         <Link href={LIST_A_SOFTWARE_PATH}>
