2206921c634a14f351f26d620ff540ff7421845f
adityachhaparia <adityachhaparia101@gmail.com>
2022-01-06 13:10:03 +0530

    Fix session warnings and typescript warnings in _app.tsx

--- src/components/freshchat-container/index.ts
+++
@@ -1,1 +0,0 @@
-export { FreshChat } from './freshchat'
--- src/lib/ga/index.ts
+++ src/lib/ga/index.ts
@@ -1,12 +1,15 @@
 declare const window: any
 // log the pageview with their URL
-export const pageview = (url) => {
+export const pageview: GA_PageView = (url) => {
   window.gtag('config', 'G-CJ16362SSH', {
     page_path: url,
   })
 }
 
 // log specific events happening.
-export const event = ({ action, params }) => {
+export const event: GA_Event = ({ action, params }) => {
   window.gtag('event', action, params)
 }
+
+export type GA_PageView = (url: any) => void
+export type GA_Event = (object: { action: any; params: any }) => void
