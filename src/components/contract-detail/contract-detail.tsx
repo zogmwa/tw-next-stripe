5aca86af0fe2aa2cd59b53ddc14a79179a9ed23d
zogmwa <zogmwatech@gmail.com>
2022-01-06 11:56:43 -0800

    Updated contract detail component

--- src/components/contract-detail/contract-detail.tsx
+++ src/components/contract-detail/contract-detail.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { BiDollar } from 'react-icons/bi'
+import Markdown from 'marked-react'
+import Lowlight from 'react-lowlight'
 import Breadcrumbs from '@mui/material/Breadcrumbs'
+import javascript from 'highlight.js/lib/languages/javascript'
 import { useRouter } from 'next/router'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 import { solutionContract } from '../../types/contracts'
+import style from '../solution-detail-introduction/style.module.scss'
+import { SolutionFAQ } from '../solution-detail-introduction'
+
+Lowlight.registerLanguage('js', javascript)
+
+const renderer = {
+  code: (snippet, lang) => {
+    return <Lowlight language={lang} value={snippet} />
+  },
+}
 
 type ContractDetailProps = {
   contractData: solutionContract
@@ -104,24 +117,67 @@ function ContractDetailComponent({ contractData }: ContractDetailProps) {
             <span className="pl-2 text-sm text-text-secondary">
               {contractData.solution.point_of_contact?.first_name ?? ''}{' '}
               {contractData.solution.point_of_contact?.last_name ?? ''}
+              {contractData.solution.point_of_contact?.email ? (
+                <span className="ml-2">{`Contact email: ${contractData.solution.point_of_contact?.email}`}</span>
+              ) : (
+                ''
+              )}
             </span>
           </>
         )}
       </div>
       <div className="flex my-4">
         <span className="w-full">
-          Started at: <b>{startedDate}</b>
+          <b className="mr-2">Started at:</b> {startedDate ? startedDate : 'Not started yet...'}
         </span>
         {startedDate ? (
           <span className="w-full">
-            Updated at: <b>{updatedDate}</b>
+            <b className="mr-2">Updated at:</b> {updatedDate}
           </span>
         ) : null}
       </div>
       <div className="flex">
         <span className="w-full">
-          Provider Notes: <b>{contractData.provider_notes}</b>
+          <b className="mr-2">Provider Notes:</b>{' '}
+          {contractData.provider_notes ?? `It will tasks ${contractData.solution.eta_days} business days.`}
         </span>
+      </div>
+      {contractData.solution.type === 'C' && (
+        <div className="flex mt-4">
+          <span className="flex flexw-full">
+            <b className="mr-2">Consultation Link:</b>{' '}
+            {contractData.solution?.consultation_scheduling_link ? (
+              <a href={contractData.solution?.consultation_scheduling_link} target="_blank">
+                {contractData.solution?.consultation_scheduling_link}
+              </a>
+            ) : (
+              'No scheduled yet...'
+            )}
+          </span>
+        </div>
+      )}
+      <div className="flex flex-col p-4 md:p-0">
+        <div id="solutions-overview" className="flex flex-col pt-2 md:pt-6">
+          <a href="#solutions-overview">
+            <h4 className="font-bold text-black text-md">Overview</h4>
+          </a>
+          <div className={style.unsetTailwind}>
+            <Markdown value={contractData.solution.description} renderer={renderer} />
+          </div>
+        </div>
+        <div style={{ scrollMarginTop: '3rem' }} id="solutions-scope" className="flex flex-col pt-2 md:pt-6">
+          <a href="#solutions-scope">
+            <h4 className="font-bold text-black text-md">Scope of Work</h4>
+          </a>
+          <div className={style.unsetTailwind}>
+            <Markdown value={contractData.solution.scope_of_work} renderer={renderer} />
+          </div>
+        </div>
+        {contractData.solution.questions.length > 0 && (
+          <div style={{ scrollMarginTop: '3rem' }} id="solutions-faq" className="py-4">
+            <SolutionFAQ questions={contractData.solution.questions} solutionSlug={contractData.solution.slug} />
+          </div>
+        )}
       </div>
     </div>
   )
