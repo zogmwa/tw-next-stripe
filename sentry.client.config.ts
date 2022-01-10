07dc3136c7707cd506f6df4186558aa2c4f32428
Sarthak Agarwal <sarthak.agarwal.cse12@iitbhu.ac.in>
2022-01-11 04:30:26 +0530

    1st Step in Direction to Strict Mode
    Merging Small changes, please review and comment on the PR if any changes are required.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,7 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs'
 
-const SENTRY_DSN: string = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+const SENTRY_DSN: string = (process.env.SENTRY_DSN as string) || (process.env.NEXT_PUBLIC_SENTRY_DSN as string)
 
 if (process.env.IS_SENTRY_ENABLED === 'true') {
   Sentry.init({
