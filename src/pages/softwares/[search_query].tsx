8840c1afc82f7827b0791677f9dc1dfb6d00bb94
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2022-01-11 21:19:32 +0530

    reduced error text size and removed pagination arrows when not needed in software listing page

--- src/pages/softwares/[search_query].tsx
+++ src/pages/softwares/[search_query].tsx
@@ -155,116 +155,119 @@ export default function ServiceList({
             router.push(`/softwares/${selectedTag}`)
           }}
         />
-        {error && <div className="font-medium text-center text-red-500">{error}</div>}
+        {error && <div className="text-sm font-medium text-center text-red-500">{error}</div>}
         {!error && (
-          <div className="flex flex-row space-x-2">
-            <div className="hidden w-1/4 space-y-4 md:flex md:flex-col">
-              <div className="border rounded">
-                <SortServiceList
-                  defaultValue={order}
-                  onChange={(value) => {
-                    let query = `/softwares/${tags}`
-                    if (value) {
-                      query += `?order=${value}`
-                      if (free_trial) {
-                        query += `&free_trial=${free_trial}`
-                      }
-                    } else {
-                      if (free_trial) {
-                        query += `?free_trial=${free_trial}`
-                      }
-                    }
-                    router.push(query)
-                  }}
-                />
-              </div>
-              <div className="border rounded">
-                <FilterServiceList
-                  defaultValue={free_trial}
-                  onChange={(value) => {
-                    let query = `/softwares/${tags}`
-                    if (value) {
-                      query += `?free_trial=${value}`
-                      if (order) {
-                        query += `&order=${order}`
-                      }
-                    } else {
-                      if (order) {
-                        query += `?order=${order}`
-                      }
-                    }
-                    router.push(query)
-                  }}
-                />
-              </div>
-            </div>
-            <div className="md:w-3/4">
-              <div className="flex justify-between mb-2">
-                <h1 className="text-xl font-medium text-text-primary">
-                  Software tagged with {tags.split(',').join(', ')}
-                </h1>
-                <div className="flex items-center justify-center space-x-4">
-                  <div className="text-sm text-text-secondary">
-                    {totalCount} {totalCount === 1 ? 'software' : 'software options'}
-                  </div>
-                  <div className="md:hidden">
-                    <MobileViewSortAndFilterServiceList
-                      defaultSortValue={order}
-                      onSortChange={(value) => {
-                        let query = `/softwares/${tags}`
-                        if (value) {
-                          query += `?order=${value}`
-                          if (free_trial) {
-                            query += `&free_trial=${free_trial}`
-                          }
-                        } else {
-                          if (free_trial) {
-                            query += `&free_trial=${free_trial}`
-                          }
+          <>
+            <div className="flex flex-row space-x-2">
+              <div className="hidden w-1/4 space-y-4 md:flex md:flex-col">
+                <div className="border rounded">
+                  <SortServiceList
+                    defaultValue={order}
+                    onChange={(value) => {
+                      let query = `/softwares/${tags}`
+                      if (value) {
+                        query += `?order=${value}`
+                        if (free_trial) {
+                          query += `&free_trial=${free_trial}`
                         }
-                        router.push(query)
-                      }}
-                      defaultFilterValue={free_trial}
-                      onFilterChange={(value) => {
-                        let query = `/softwares/${tags}`
-                        if (value) {
-                          query += `?free_trial=${value}`
-                          if (order) {
-                            query += `&order=${order}`
-                          }
-                        } else {
-                          if (order) {
-                            query += `?order=${order}`
-                          }
+                      } else {
+                        if (free_trial) {
+                          query += `?free_trial=${free_trial}`
                         }
-                        router.push(query)
-                      }}
-                    />
-                  </div>
+                      }
+                      router.push(query)
+                    }}
+                  />
+                </div>
+                <div className="border rounded">
+                  <FilterServiceList
+                    defaultValue={free_trial}
+                    onChange={(value) => {
+                      let query = `/softwares/${tags}`
+                      if (value) {
+                        query += `?free_trial=${value}`
+                        if (order) {
+                          query += `&order=${order}`
+                        }
+                      } else {
+                        if (order) {
+                          query += `?order=${order}`
+                        }
+                      }
+                      router.push(query)
+                    }}
+                  />
                 </div>
               </div>
-              <div className="max-w-full px-2 mb-2 border rounded-md">
-                <ul className="flex flex-col justify-start pb-8 divide-y divide">
-                  {services.map((service, index) => {
-                    const isChecked = !!checkedList.find((item) => item.slug === service.slug)
-                    return (
-                      <li
-                        key={index}
-                        className="max-w-full mt-2 transition duration-500 ease-in-out bg-background-surface hover:bg-background-light"
-                      >
-                        <ServiceCard service={service} onToggleCompare={handleChecked} isChecked={isChecked} />
-                      </li>
-                    )
-                  })}
-                </ul>
-                <CompareAccordian checkedList={checkedList} onServiceRemove={handleServiceRemove} />
+              <div className="md:w-3/4">
+                <div className="flex justify-between mb-2">
+                  <h1 className="text-xl font-medium text-text-primary">
+                    Software tagged with {tags.split(',').join(', ')}
+                  </h1>
+                  <div className="flex items-center justify-center space-x-4">
+                    <div className="text-sm text-text-secondary">
+                      {totalCount} {totalCount === 1 ? 'software' : 'software options'}
+                    </div>
+                    <div className="md:hidden">
+                      <MobileViewSortAndFilterServiceList
+                        defaultSortValue={order}
+                        onSortChange={(value) => {
+                          let query = `/softwares/${tags}`
+                          if (value) {
+                            query += `?order=${value}`
+                            if (free_trial) {
+                              query += `&free_trial=${free_trial}`
+                            }
+                          } else {
+                            if (free_trial) {
+                              query += `&free_trial=${free_trial}`
+                            }
+                          }
+                          router.push(query)
+                        }}
+                        defaultFilterValue={free_trial}
+                        onFilterChange={(value) => {
+                          let query = `/softwares/${tags}`
+                          if (value) {
+                            query += `?free_trial=${value}`
+                            if (order) {
+                              query += `&order=${order}`
+                            }
+                          } else {
+                            if (order) {
+                              query += `?order=${order}`
+                            }
+                          }
+                          router.push(query)
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="max-w-full px-2 mb-2 border rounded-md">
+                  <ul className="flex flex-col justify-start pb-8 divide-y divide">
+                    {services.map((service, index) => {
+                      const isChecked = !!checkedList.find((item) => item.slug === service.slug)
+                      return (
+                        <li
+                          key={index}
+                          className="max-w-full mt-2 transition duration-500 ease-in-out bg-background-surface hover:bg-background-light"
+                        >
+                          <ServiceCard service={service} onToggleCompare={handleChecked} isChecked={isChecked} />
+                        </li>
+                      )
+                    })}
+                  </ul>
+                  <CompareAccordian checkedList={checkedList} onServiceRemove={handleServiceRemove} />
+                </div>
               </div>
             </div>
-          </div>
+
+            <div className="flex justify-end">
+              <Pagination page={currentPage} count={pageCount} onChange={handlePagination} />
+            </div>
+          </>
         )}
-        <div className="flex justify-end">
-          <Pagination page={currentPage} count={pageCount} onChange={handlePagination} />
-        </div>
       </div>
     </>
   )
