248af26de4d8842094b6e7f4b0d5e2fba28eab96
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2022-01-13 22:32:31 +0530

    updated about page stylings

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -38,60 +38,62 @@ export default function About() {
     <>
       <DynamicHeader title="About - Taggedweb" description="Find best software solutions provided by domain experts" />
       <div className="flex flex-col justify-center">
-        <div className="flex flex-col justify-center divide-y">
-          <div className="self-center pt-6 pb-8 space-y-2 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              About
-            </h1>
-          </div>
-          <div className="flex items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-            <div className="flex flex-col items-center pt-8 space-x-2">
-              <Image
-                src={taggedwebAboutDetails.avatar}
-                alt="avatar"
-                width="192px"
-                height="192px"
-                className="w-48 h-48 rounded-full"
-              />
-              <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{taggedwebAboutDetails.name}</h3>
-              <div className="text-gray-500 dark:text-gray-400">{taggedwebAboutDetails.occupation}</div>
-              <div className="flex pt-6 space-x-3">
-                <a href={`mailto:${taggedwebAboutDetails.email}`}>
-                  <TiMail className="p-1 text-3xl border border-solid rounded-full border-border-default" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/taggedweb/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-900 no-underline"
-                >
-                  <TiSocialLinkedin className="p-1 text-3xl border border-solid rounded-full border-border-default" />
-                </a>
-                <a
-                  href="https://twitter.com/TheTaggedWeb"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-900 no-underline"
-                >
-                  <TiSocialTwitter className="p-1 text-3xl border border-solid rounded-full border-border-default" />
-                </a>
-                <a
-                  href="https://www.facebook.com/taggedweb/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-900 no-underline"
-                >
-                  <TiSocialFacebook className="p-1 text-3xl border border-solid rounded-full border-border-default" />
-                </a>
+        <div className="flex flex-col items-center justify-center p-6 space-y-2">
+          <h1 className="p-2 font-serif text-2xl text-gray-700 border-b-2 border-neutral-800 sm:text-4xl md:text-6xl">
+            About Us
+          </h1>
+          <div className="flex flex-col items-center w-full">
+            <div className="flex flex-col-reverse items-center justify-between w-full space-x-2 md:flex-row lg:w-3/4 lg:space-x-4">
+              <div className="flex flex-col items-center w-1/2 mx-1 space-x-1">
+                <Image
+                  src={taggedwebAboutDetails.avatar}
+                  alt="avatar"
+                  width="180px"
+                  height="180px"
+                  className="w-40 h-40 rounded-full"
+                />
+                <h3 className="pb-2 text-base font-bold tracking-tight md:text-xl xl:text-2xl">
+                  {taggedwebAboutDetails.name}
+                </h3>
+                <div className="text-xs text-gray-500 lg:text-sm dark:text-gray-400 ">
+                  {taggedwebAboutDetails.occupation}
+                </div>
+                <div className="flex pt-2 space-x-2 md:space-x-3 md:pt-3">
+                  <a href={`mailto:${taggedwebAboutDetails.email}`}>
+                    <TiMail className="p-1 text-2xl border border-solid rounded-full md:text-3xl border-border-default" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/company/taggedweb/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-900 no-underline"
+                  >
+                    <TiSocialLinkedin className="p-1 text-2xl border border-solid rounded-full md:text-3xl border-border-default" />
+                  </a>
+                  <a
+                    href="https://twitter.com/TheTaggedWeb"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-900 no-underline"
+                  >
+                    <TiSocialTwitter className="p-1 text-2xl border border-solid rounded-full md:text-3xl border-border-default" />
+                  </a>
+                  <a
+                    href="https://www.facebook.com/taggedweb/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-900 no-underline"
+                  >
+                    <TiSocialFacebook className="p-1 text-2xl border border-solid rounded-full md:text-3xl border-border-default" />
+                  </a>
+                </div>
               </div>
-            </div>
-            <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
-              {taggedwebAboutDetails.Description}
+              <div className="prose dark:prose-dark max-w-none">{taggedwebAboutDetails.Description}</div>
             </div>
           </div>
         </div>
-        <div className="container py-12">
-          <div className="flex flex-wrap justify-center -m-4">
+        <div className="py-12">
+          <div className="flex flex-wrap items-center justify-center">
             {servicesCategoryData.map((d) => (
               <Card key={d.title} title={d.title} description={d.description} imgSrc={d.imgSrc} href={d.href} />
             ))}
