696e8ca57df0120718d5c4e7ab6785f9b1681329
zogmwa <zogmwatech@gmail.com>
2022-01-14 05:11:05 -0800

    Updated stripe add card component

--- src/components/add-card-detail/add-card.tsx
+++ src/components/add-card-detail/add-card.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Elements, ElementsConsumer, CardElement } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
+import * as stripeJs from '@stripe/stripe-js'
 import { Button } from '../button'
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLISH_KEY)
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = stripeJs.loadStripe(process.env.STRIPE_PUBLISH_KEY)
 
-function AddCardForm(props: any) {
-  const handleSubmit = async () => {
-    const { elements, stripe } = props
-    const cardElement = elements.getElement(CardElement)
+const CheckoutForm = (props: any) => {
+  // const stripe = useStripe();
+  // const elements = useElements();
+  const { elements, stripe } = props
 
+  const handleSubmit = async (event) => {
+    // Block native form submission.
+    event.preventDefault()
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const card = elements.getElement(CardElement)
+
+    if (card == null) {
+      return
+    }
+
+    // Use your card Element with other Stripe.js APIs
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
-      card: cardElement,
+      card,
     })
 
     if (error) {
@@ -24,20 +46,36 @@ function AddCardForm(props: any) {
   }
 
   return (
-    <div className="flex flex-col">
-      <h1>stripe form</h1>
-      <CardElement className="text-md" />
-      <Button className="self-end" onClick={handleSubmit}>
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <Button type="submit" className="mt-2" disabled={!stripe}>
         Add Card
       </Button>
-    </div>
+    </form>
   )
 }
 
-export function AddCard() {
+const AddCardComponent = () => {
   return (
     <Elements stripe={stripePromise}>
-      <ElementsConsumer>{(ctx: any) => <AddCardForm {...ctx} />}</ElementsConsumer>
+      <ElementsConsumer>{(ctx: any) => <CheckoutForm {...ctx} />}</ElementsConsumer>
     </Elements>
   )
 }
+
+export const AddCard = AddCardComponent
