6c95dcc5ee42adc59027f0e81ec91e873a50c372
zogmwa <zogmwatech@gmail.com>
2022-01-13 17:16:11 -0800

    Added add card detail component to storybook

--- src/components/add-card-detail/add-card-detail.stories.tsx
+++ src/components/add-card-detail/add-card-detail.stories.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Meta } from '@storybook/react/types-6-0'
 import { AddCardDetail } from './add-card-detail'
+import { AddCard } from './add-card'
 
 export default {
   title: 'General/AddCardDetail',
   component: AddCardDetail,
 } as Meta
+
+export function StripeAddCardDetail() {
+  return <AddCard />
+}
 
 export function DefaultAddCardDetail() {
   return <AddCardDetail addCardFunction={() => {}} />
