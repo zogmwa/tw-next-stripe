78cf95595e05c1c435c2d986f36c3ad4cfc95a51
zogmwa <zogmwatech@gmail.com>
2022-01-16 11:57:28 -0800

    Implemented fetch has_payment_method field API

---
+++ src/pages/api/user/payment_method/has_payment_method.ts
@@ -0,0 +1,21 @@
+import { withApiAuthRequired } from '@taggedweb/utils/auth-wrappers'
+import { clientWithRetries } from '@taggedweb/utils/clientWithRetries'
+import { getAccessToken } from '@taggedweb/utils/token'
+import { withSentry } from '@sentry/nextjs'
+
+/**
+ * API Route handler for get has_payment_method field of user.
+ */
+export default withSentry(
+  withApiAuthRequired(async (req, res) => {
+    if (req.method === 'GET') {
+      const access = await getAccessToken(req)
+      const { data } = await clientWithRetries.get('/users/get_has_payment_method/', {
+        headers: {
+          Authorization: `Bearer ${access}`,
+        },
+      })
+      res.json(data)
+    }
+  }),
+)
