6d51d099d4d60746331643d2fbfe9cfd680165f3
zogmwa <zogmwatech@gmail.com>
2022-01-17 17:39:48 -0800

    Updated check payment method API to fetch payment method list API

---
+++ src/pages/api/user/payment_method/payment_method_list.ts
@@ -0,0 +1,21 @@
+import { withApiAuthRequired } from '@taggedweb/utils/auth-wrappers'
+import { clientWithRetries } from '@taggedweb/utils/clientWithRetries'
+import { getAccessToken } from '@taggedweb/utils/token'
+import { withSentry } from '@sentry/nextjs'
+
+/**
+ * API Route handler for get payment_method_list of user.
+ */
+export default withSentry(
+  withApiAuthRequired(async (req, res) => {
+    if (req.method === 'GET') {
+      const access = await getAccessToken(req)
+      const { data } = await clientWithRetries.get('/users/payment_method_list/', {
+        headers: {
+          Authorization: `Bearer ${access}`,
+        },
+      })
+      res.json(data)
+    }
+  }),
+)
