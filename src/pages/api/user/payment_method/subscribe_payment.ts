c3bec85510ab0b5b5b2380dac2ef0ee6f36119b9
zogmwa <zogmwatech@gmail.com>
2022-01-17 17:40:15 -0800

    Implemented subscribe payment API

---
+++ src/pages/api/user/payment_method/subscribe_payment.ts
@@ -0,0 +1,21 @@
+import { withApiAuthRequired } from '@taggedweb/utils/auth-wrappers'
+import { clientWithRetries } from '@taggedweb/utils/clientWithRetries'
+import { getAccessToken } from '@taggedweb/utils/token'
+import { withSentry } from '@sentry/nextjs'
+
+/**
+ * API Route handler for subscribe payment for metered billing solution.
+ */
+export default withSentry(
+  withApiAuthRequired(async (req, res) => {
+    if (req.method === 'POST') {
+      const access = await getAccessToken(req)
+      const { data } = await clientWithRetries.post('/users/subscribe_payment/', req.body, {
+        headers: {
+          Authorization: `Bearer ${access}`,
+        },
+      })
+      res.json(data)
+    }
+  }),
+)
