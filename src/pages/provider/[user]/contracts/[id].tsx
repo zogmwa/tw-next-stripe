cf8df4d679b64c2bd826b93e42ffa56ad7028a48
zogmwa <zogmwatech@gmail.com>
2022-01-25 15:37:05 -0800

    Added provider's contract detail page.

---
+++ src/pages/provider/[user]/contracts/[id].tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import * as Sentry from '@sentry/nextjs'
+import { ProviderContractDetail } from '@taggedweb/components/provider-contract-detail'
+import { withSessionSSR } from '@taggedweb/utils/session'
+import { fetchProviderContract } from '@taggedweb/solution-queries/fetch-provider-contract'
+
+export const getServerSideProps = withSessionSSR(async (context) => {
+  const {
+    query: { user, id },
+  } = context
+
+  try {
+    const trackingData = await fetchProviderContract(context.req, user, id)
+    if (trackingData.length < 1) {
+      return {
+        notFound: true,
+      }
+    }
+    return {
+      props: { trackingData },
+    }
+  } catch (err) {
+    Sentry.captureException(err)
+    return {
+      props: {},
+    }
+  }
+})
+
+export default function TrackingTime({ trackingData }) {
+  console.log('trackingData:', trackingData)
+  return (
+    <div id="contracts" className="flex flex-col max-w-screen-lg mx-auto min-h-[50%]">
+      <ProviderContractDetail trackingData={trackingData} />
+    </div>
+  )
+}
