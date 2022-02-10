3ceb3bb227e23158cfe07248db05372b412d35ab
zogmwa <zogmwatech@gmail.com>
2022-02-10 02:12:40 +0100

    Integrated API for fetch payment method of partner's customers

--- src/pages/api/user/payment_method/payment_method_list.ts
+++ src/pages/api/user/payment_method/payment_method_list.ts
@@ -1,5 +1,5 @@
-import { withApiAuthRequired } from '@taggedweb/utils/auth-wrappers'
-import { clientWithRetries } from '@taggedweb/utils/clientWithRetries'
+import { withSessionApi } from '@taggedweb/utils/session'
+import { serverSideClient } from '@taggedweb/utils/client'
 import { getAccessToken } from '@taggedweb/utils/token'
 import { withSentry } from '@sentry/nextjs'
 
@@ -7,14 +7,21 @@ import { withSentry } from '@sentry/nextjs'
  * API Route handler for get payment_methods of user.
  */
 export default withSentry(
-  withApiAuthRequired(async (req, res) => {
+  withSessionApi(async (req, res) => {
     if (req.method === 'GET') {
+      const { customer_id } = req.query
       const access = await getAccessToken(req)
-      const { data } = await clientWithRetries.get('/users/payment_methods/', {
-        headers: {
-          Authorization: `Bearer ${access}`,
-        },
-      })
+      const config = access
+        ? {
+            headers: {
+              Authorization: `Bearer ${access}`,
+            },
+          }
+        : null
+      const { data } = await serverSideClient(req).get(
+        customer_id ? `/users/payment_methods?customer_id=${customer_id}` : '/users/payment_methods/',
+        config,
+      )
       res.json(data)
     }
   }),
