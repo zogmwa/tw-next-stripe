a09c39149110bc69abe291f3f0155635ad57ae12
zogmwa <zogmwatech@gmail.com>
2022-02-10 02:12:22 +0100

    Integrated API for attach payment method of partner's customers

---
+++ src/pages/api/user/payment_method/attach_for_partner.ts
@@ -0,0 +1,25 @@
+import { withSessionApi } from '@taggedweb/utils/session'
+import { serverSideClient } from '@taggedweb/utils/client'
+import { getAccessToken } from '@taggedweb/utils/token'
+import { withSentry } from '@sentry/nextjs'
+
+/**
+ * Our Partner's customers could attach payment method.
+ */
+
+export default withSentry(
+  withSessionApi(async (req, res) => {
+    if (req.method === 'POST') {
+      const access = await getAccessToken(req)
+      const config = access
+        ? {
+            headers: {
+              Authorization: `Bearer ${access}`,
+            },
+          }
+        : null
+      const { data } = await serverSideClient(req).post(`/users/attach_card_for_partners/`, req.body, config)
+      return res.json(data)
+    }
+  }),
+)
