cd531c972842af466be1b2bc7756600e0793e06f
zogmwa <zogmwatech@gmail.com>
2022-02-12 17:54:10 +0100

    Implemented API for subscribe asset price

---
+++ src/pages/api/user/payment_method/subscribe_asset_price.ts
@@ -0,0 +1,25 @@
+import { withSessionApi } from '@taggedweb/utils/session'
+import { serverSideClient } from '@taggedweb/utils/client'
+import { getAccessToken } from '@taggedweb/utils/token'
+import { withSentry } from '@sentry/nextjs'
+
+/**
+ * Our Partner's customers could subscribe asset price.
+ */
+
+export default withSentry(
+  withSessionApi(async (req, res) => {
+    if (req.method === 'POST') {
+      const access = await getAccessToken(req)
+      const config = access
+        ? {
+            headers: {
+              Authorization: `Bearer ${access}`,
+            },
+          }
+        : null
+      const { data } = await serverSideClient(req).post(`/users/subscribe_asset_price_plan/`, req.body, config)
+      return res.json(data)
+    }
+  }),
+)
