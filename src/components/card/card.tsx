22f27cfa1514161109533c8efabcb1e8903ff2aa
zogmwa <zogmwatech@gmail.com>
2022-02-25 00:27:19 +0100

    Updated card component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,45 +2,41 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Card = ({ title, description, imgSrc, href }) => (
-  <div className="p-4 md:w-1/2 md" style={{ maxWidth: '544px' }}>
-    <div className="h-full overflow-hidden border border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
-      {href ? (
-        <Link href={href} aria-label={`Link to ${title}`}>
-          <a>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center lg:h-48 md:h-36"
-              width={544}
-              height={306}
-            />
-          </a>
-        </Link>
-      ) : (
-        <Image
-          alt={title}
-          src={imgSrc}
-          className="object-cover object-center lg:h-48 md:h-36"
-          width={544}
-          height={306}
-        />
-      )}
+const Card = ({ subTitle = '', title, description = '', imgSrc = '', href = '', linkTitle = '' }) => (
+  <div className="p-4 md:w-1/2 max-w-[554px]">
+    <div className="h-full overflow-hidden border border-border-default rounded-md box-shadow shadow-md">
+      {imgSrc &&
+        (href ? (
+          <Link href={href} aria-label={`Link to ${title}`}>
+            <a>
+              <Image
+                alt={title}
+                src={imgSrc}
+                className="object-cover object-center lg:h-48 md:h-36"
+                width={544}
+                height={306}
+              />
+            </a>
+          </Link>
+        ) : (
+          <Image
+            alt={title}
+            src={imgSrc}
+            className="object-cover object-center lg:h-48 md:h-36"
+            width={544}
+            height={306}
+          />
+        ))}
       <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              <a>{title}</a>
-            </Link>
-          ) : (
-            title
-          )}
-        </h2>
-        <p className="mb-3 prose text-gray-500 max-w-none dark:text-gray-400">{description}</p>
+        {subTitle && <span className="mb-2 text-sm text-text-primary">{subTitle}</span>}
+        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">{title}</h2>
+        {description && (
+          <p className="mb-3 font-medium text-sm prose text-gray-500 max-w-none dark:text-gray-400">{description}</p>
+        )}
         {href && (
           <Link href={href} aria-label={`Link to ${title}`}>
-            <a className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-              Learn more &rarr;
+            <a className="mb-3 text-sm font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+              {linkTitle} &rarr;
             </a>
           </Link>
         )}
